e9bf79d7af4a676037f1f40897fbb9602e40c655
Orphé Houeto <orphehoueto9@gmail.com>
2024-05-16 15:31:15 +0100

    mise en place des functions register() et handleregister() pour assurer l'inscription

---
+++ app/Http/Controllers/UseAuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\UserAuthRequest;
+
+class UseAuthController extends Controller
+{
+    public function register()
+    {
+        return view('auth/users/register');
+    }
+
+    public function login(){
+        return view('auth/users/login');
+    }
+
+    public function handleregister(UserAuthRequest $request)
+    {
+        try {
+            User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+            return redirect()->back()->with('success', 'Votre compte a été crée, connectez vous');
+        } catch (Exception $e) {
+            dd($e);
+        }
+    }
+}
