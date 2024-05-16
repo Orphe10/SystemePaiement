6b4127a3bed8f9ec0e770360c574fd5ab665370b
Orphé Houeto <orphehoueto9@gmail.com>
2024-05-16 15:51:45 +0100

    Ajout des routes pour la connextion

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\UseAuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Inscription
+Route::get('/register',[UseAuthController::class,'register'])->name('user-register');
+Route::post('/register',[UseAuthController::class,'handleregister'])->name('HandleUserRegister');
+
+//Connexion
+Route::get('/login',[UseAuthController::class,'login'])->name('user_login');
+Route::post('/login',[UseAuthController::class,'handellogin'])->name('HandleUserLogin');
