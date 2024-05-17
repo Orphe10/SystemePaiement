387e41b4b78f307f26cc70d8d1d52728423c0fbc
Orphé Houeto <orphehoueto9@gmail.com>
2024-05-17 17:48:59 +0100

    Creation du fichier VendorLoginRequest.php pour la verification des informations envoyées depuis le formulaire de connexion des vendeurs

---
+++ app/Http/Requests/VendorLoginRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class VendorLoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|exists:vendors,email',
+            'password' => 'required|min:4'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'email.required' => 'Le mail est requis',
+            'email.email' => 'Le email n\'est pas correct',
+            'email.exists' => 'Cette adresse email n\'est pas reconnu',
+            'password.required' => 'Le password est requis',
+            'password.min' => 'Le mot de passe doit contenir au moins 4 caracteres',
+        ];
+    }
+}
